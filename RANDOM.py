79cd235c5ecf58dcf779c32483eb2796ab5bb098
keerthanam1912 <106869388+keerthanam1912@users.noreply.github.com>
2022-06-17 22:43:01 +0600

    Update RANDOM.py

--- RANDOM.py
+++ RANDOM.py
@@ -1,15 +1,16 @@
 import random
 import string
-low=string.ascii_lowercase
-upp=string.ascii_uppercase
-dig=string.digits[1:]
-sym=["!","@",'#','$']
-tot=random.choice(low)+random.choice(upp)+random.choice(sym)+random.choice(dig)
-length=int(input("Enter Length Of Password:"))
-a=''
-for i in range(length-4):
-    a+=random.choice(dig)
-print(f"Your Password is:{a+tot}")
-
-    
-
+n=int(input("Number of passwords you required:"))
+length=int(input(f"Enter the length of all {n} passwords:"))
+x=0
+while(n>=x):
+    low=string.ascii_lowercase
+    upp=string.ascii_uppercase
+    dig=string.digits[1:]
+    sym=["!","@",'#','$']
+    tot=random.choice(low)+random.choice(upp)+random.choice(sym)+random.choice(dig)
+    a=''
+    for i in range(length-4):
+        a+=random.choice(dig)
+    print(f"Your Password is:{a+tot}")
+    x+=1
